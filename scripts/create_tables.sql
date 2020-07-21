e624abb5d245808c37ec348863cdfe9fe9cd4b48
alperdomo <alvaro_perdomo@outlook.com>
2020-07-21 15:54:47 +0200

    Updated sql file, renamed datetime.csv files

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -16,19 +16,20 @@ CREATE TABLE ratings (
 	user_id VARCHAR,
 	movie_id VARCHAR,
 	rating REAL,
-	time_stamp VARCHAR(15),
-	FOREIGN KEY (movie_id) REFERENCES movies
+	time_stamp TIMESTAMP,
+	FOREIGN KEY (user_id) REFERENCES movies
 );
 
 CREATE TABLE tags (
 	user_id VARCHAR,
 	movie_id VARCHAR NOT NULL,
 	tag VARCHAR(100),
-	time_stamp VARCHAR(15)
+	time_stamp TIMESTAMP
 );
+
 
 -- Inster data into the table
 COPY movies FROM '../ml-latest-small/movies.csv' DELIMITER ',' CSV HEADER;
 COPY ratings FROM '../data/ml-latest-small/ratings.csv' DELIMITER ',' CSV HEADER;
-COPY tags FROM '../data/ml-latest-small/tags.csv' DELIMITER ',' CSV HEADER;
 COPY links FROM '../data/ml-latest-small/links.csv' DELIMITER ',' CSV HEADER;
+COPY tags FROM '../data/ml-latest-small/tags.csv' DELIMITER ',' CSV HEADER;
