795691f903f73310a709d66f1e1294b320ddab0d
senzelden <sunless@gmx.net>
2020-07-22 17:21:11 +0200

    Train model on KNNimputer, make other alternatives optional

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,10 +1,20 @@
 import pandas as pd
-from sklearn.decomposition import NMF
 from joblib import dump
+from sklearn.decomposition import NMF
+from sklearn.impute import KNNImputer
 
 
-def train_model():
-    """returns movie recommendations dictionary with movie titles and predicted ratings based on nmf model"""
+
+def train_model(n_features=20, fill_method='KNNimputer', fill_value=2.5):
+    """
+    returns movie recommendations dictionary with movie titles and predicted ratings based on nmf model
+
+    PARAMS
+    -------
+    n_features (optiona): number of features to be used for NMF (default is 20)
+    fill_method (optional): either 'single_value' or 'KNNimputer' (default)
+    fill_value (optional): if 'single_value' is used as fill_method, fill_value can be set manually (e.g. 0, 2.5, 3)
+    """
     # Read the data
     movies = pd.read_csv("data/ml-latest-small/movies.csv")
     ratings = pd.read_csv("data/ml-latest-small/ratings.csv")
@@ -13,10 +23,14 @@ def train_model():
     df = pd.merge(ratings, movies, "outer", on="movieId")
     rtrue = df[["userId", "movieId", "rating"]].set_index("userId")
     rtrue = rtrue.pivot(index=rtrue.index, columns="movieId").copy()
-    rtrue_fill = rtrue.fillna(2.5).copy()
+    if fill_method == 'KNNimputer':
+        imputer = KNNImputer(n_neighbors=5)
+        rtrue_fill = pd.DataFrame(imputer.fit_transform(rtrue), columns=rtrue.columns, index=rtrue.index)
+    else:
+        rtrue_fill = rtrue.fillna(fill_value).copy()
 
     # Run model
-    m = NMF(12)
+    m = NMF(n_features, max_iter=1000)
     m.fit(rtrue_fill)  # Slowest part of the code
     dump(m, "../data/nmf_model.joblib")
 
