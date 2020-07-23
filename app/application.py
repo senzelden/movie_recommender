072e2d233843ca31eab82c2833cc604d282cb9e9
senzelden <sunless@gmx.net>
2020-07-23 15:23:23 +0200

    Fix minibug in application.py, update readme

--- app/application.py
+++ app/application.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", choices=MOVIES)
+    return render_template("index.html")
 
 
 @app.route("/hello/<name>")
