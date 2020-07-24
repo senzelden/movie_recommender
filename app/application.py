11b3386687a2db6069c41c8b87ea3dfcd830c9e9
alperdomo <alvaro_perdomo@outlook.com>
2020-07-24 08:23:13 +0200

    Added dropdown bottom

--- app/application.py
+++ app/application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from nmf_recommend import nmf_recommender
 from spiced_recommender import Recommender
 from extract_infos import omdb_extract, postgres_extract
-
+from cosine import #### Missing function here
 
 app = Flask(__name__)
 
@@ -28,17 +28,20 @@ def movie_details(movie_id):
 @app.route("/recommendation")
 def recommend():
     user_input = dict(request.args)
+    method_ = user_input.keys()[-1]
+    if method_[1] == "NMF":
+        # recommendations = nmf_recommender(user_input)
+        recommender = Recommender(user_input)
+        recommendations = recommender.nmf()
+        for recommended_movie_id in recommendations.keys():
+            postgres_infos = postgres_extract(recommended_movie_id)
+            imdb_id = postgres_infos[-1]
+            recommendations[recommended_movie_id]["omdb_dict"] = omdb_extract(imdb_id)
+        return render_template(
+            "recommendation.html", movies=recommendations, input=user_input
+        )
+    if if method_[1] == "cosine":
 
-    # recommendations = nmf_recommender(user_input)
-    recommender = Recommender(user_input)
-    recommendations = recommender.nmf()
-    for recommended_movie_id in recommendations.keys():
-        postgres_infos = postgres_extract(recommended_movie_id)
-        imdb_id = postgres_infos[-1]
-        recommendations[recommended_movie_id]["omdb_dict"] = omdb_extract(imdb_id)
-    return render_template(
-        "recommendation.html", movies=recommendations, input=user_input
-    )
 
 
 @app.route("/recommendation_test")
